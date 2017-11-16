4a172ce9768a8ce0284c9b3087380d404ec82d05
arturiax <arturiax@hotmail.com>
2017-11-16 08:46:47 +0000

    click

--- PVS/server.R
+++ PVS/server.R
@@ -17,11 +17,31 @@ library(ggmap)
 shinyServer(function(input, output, session) {
   
   base<-read_csv2("/home/art/shiny-server/PVS/pruevashiny.csv")
+  data=data.frame(x=c(43.13, 43.24), y=c(-2.54,-2.91), id=c("place1", "place2"))
+  
+  
+  data_of_click <- reactiveValues(clickedMarker=NULL)
+  observeEvent(input$map_marker_click,{
+    data_of_click$clickedMarker <- input$map_marker_click
+  })
+  
+  
+  output$plot=renderPlot({
+    my_place=data_of_click$clickedMarker$id
+    if(is.null(my_place)){my_place="place1"}
+    if(my_place=="place1"){
+      plot(rnorm(1000), col=rgb(0.9,0.4,0.1,0.3), cex=3, pch=20)
+    }else{
+      barplot(rnorm(10), col=rgb(0.1,0.4,0.9,0.3))
+    }    
+  })
+
+  
   jj<- readRDS("/home/art/shiny-server/PVS/base")
   jj$direc_corta <- gsub(", Spain","", jj$formatted_address)
   jj$popup <- paste(sep = "<br/>",paste0("<b>",jj$nombre,"</b>"),jj$direc_corta)
   icons <- awesomeIcons(
-    icon = ifelse(jj$accuracy== "bar", 'fa-beer', "medkit"),
+    icon = "medkit",
     iconColor = 'black',
     library = 'fa',
     markerColor = ifelse(rownames(jj) %in% c(1,2,6), "blue", "green")
@@ -31,6 +51,7 @@ shinyServer(function(input, output, session) {
   
   output$mymap <- renderLeaflet({
    leaflet(jj) %>%
+      setView(zoom=4)  %>% 
     addTiles() %>%  # Add default OpenStreetMap map tiles
    addAwesomeMarkers(lng=~long, lat=~lat, icon = icons, popup=~popup, label=~nombre) 
   })
