917c0855c4090c3716cca889905b37c6f9be8a20
arturiax <arturiax@hotmail.com>
2017-11-17 09:30:49 +0000

    uuiiuuiuiiuuiuiiuiuiu

--- PVS/server.R
+++ PVS/server.R
@@ -17,7 +17,9 @@ library(ggmap)
 shinyServer(function(input, output, session) {
   
   base<-read_csv2("/home/art/shiny-server/PVS/pruevashiny.csv")
-  data=data.frame(y=c(43, 43), x=c(-2,-3), id=c("place1", "place2"))
+  jj<- readRDS("/home/art/shiny-server/PVS/base")
+  
+  data=data.frame(x=jj$long, y=jj$lat, id=c("ELORRIO", "BOLUETA", "LANDAKO", "OTXARKOAGA", "ETXEBARRI", "ARRIGORRIAGA"))
   
   
   data_of_click <- reactiveValues(clickedMarker=NULL)
@@ -29,9 +31,9 @@ shinyServer(function(input, output, session) {
   
   
   
-  jj<- readRDS("/home/art/shiny-server/PVS/base")
-  jj$direc_corta <- gsub(", Spain","", jj$formatted_address)
-  jj$popup <- paste(sep = "<br/>",paste0("<b>",jj$nombre,"</b>"),jj$direc_corta)
+  
+  data$direc_corta <- gsub(", Spain","", jj$formatted_address)
+  data$popup <- paste(sep = "<br/>",paste0("<b>",jj$nombre,"</b>"),jj$direc_corta)
   icons <- awesomeIcons(
     icon = "medkit",
     iconColor = 'black',
@@ -44,20 +46,15 @@ shinyServer(function(input, output, session) {
     leaflet() %>%
       setView(lat=43, lng=-2.8, zoom=11)  %>% 
       addTiles(options = providerTileOptions(noWrap = TRUE)) %>%  # Add default OpenStreetMap map tiles
-      addCircleMarkers(data=data, ~x , ~y, layerId=~id, popup=~id, radius=8 , color="black",  
-                       fillColor="red", stroke = TRUE, fillOpacity = 0.8)
-    #addAwesomeMarkers(lng=~long, lat=~lat, icon = icons, popup=~popup, label=~nombre) 
+      #addCircleMarkers(data=data, ~x , ~y, layerId=~id, popup=~id, radius=8 , color="black",  fillColor="red", stroke = TRUE, fillOpacity = 0.8)
+    addAwesomeMarkers(data=data, ~x , ~y, layerId=~id, icon = icons, popup=~popup, label=~nombre) 
   })
   
-  output$plot=renderPlot({
-    my_place=data_of_click$clickedMarker$id
-    if(is.null(my_place)){my_place="place1"}
-    if(my_place=="place1"){
-      plot(rnorm(1000), col=rgb(0.9,0.4,0.1,0.3), cex=3, pch=20)
-    }else{
-      barplot(rnorm(10), col=rgb(0.1,0.4,0.9,0.3))
-    }    
-  })
+  output$plot=renderValueBox({
+  my_place=data_of_click$clickedMarker$id
+  if(is.null(my_place)){my_place="ELORRIO"}
+   valueBox(consultantes$n[consultantes$UAP==my_place][1], "Consultantes", color="blue", width=5, icon=icon("male"))
+   })
 
  
   
