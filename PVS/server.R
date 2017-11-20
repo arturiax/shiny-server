561c00a22c76b78f47f73b38397b93b64e7666b8
arturiax <arturiax@hotmail.com>
2017-11-20 11:50:41 +0000

    tytrytri ghcm ngf

--- PVS/server.R
+++ PVS/server.R
@@ -95,13 +95,8 @@ shinyServer(function(input, output, session) {
   
     
     faithful %>%
-      ggvis(~waiting, ~eruptions, fillOpacity := 0.5,
-            shape := input_select(label = "Choose shape:",
-                                  choices = c("circle", "square", "cross",
-                                              "diamond", "triangle-up", "triangle-down")),
-            fill := input_select(label = "Choose color:",
-                                 choices = c("black", "red", "blue", "green"))) %>%
-      layer_points()  
+      ggvis(~waiting, ~eruptions, fill := input_radiobuttons(label= "Choose color:", choices=  c("black", "red", "blue", "green") ) )%>%
+      layer_points()
     
   })
   
