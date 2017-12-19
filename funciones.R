329a57d2f42798536acee3fd5307d19953ce01a8
arturiax <arturiax@hotmail.com>
2017-12-19 12:02:47 +0000

    añadir actividad informe

--- funciones.R
+++ funciones.R
@@ -13,3 +13,21 @@ colnames(kk)<-c("fechas", "A1","A1_AF", "A1_DT", "A1_TB", "A2","A2_AF", "A2_DT",
 kk$fechas <- fec
 return(kk)
 }
+
+actividad <- function(z, fec) {
+  
+  
+  varis<-with(z,list(fch_a1, fch_a1_af, fch_a1_dt, fch_a1_tb, fch_a2, fch_a2_af, fch_a2_dt, fch_a2_tb, fch_a4, fch_a4_af, fch_a4_dt, fch_a4_tb))
+  
+  sacar_n<- function(y) {
+    sapply(fec, function(x) {sum(month(x)==month(y) & year(x)==year(y), na.rm= TRUE)})
+  }
+  rr<-sapply(varis, sacar_n)
+  kk<-as.data.frame(cbind(fec, rr))
+  colnames(kk)<-c("fechas", "A1","A1_AF", "A1_DT", "A1_TB", "A2","A2_AF", "A2_DT", "A2_TB", "A4","A4_AF", "A4_DT", "A4_TB")
+  kk$fechas <- fec
+  return(kk)
+}
+
+
+
